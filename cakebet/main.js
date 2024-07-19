a7a034c3ebea44c2f44ca909725de853685bd719
nox-shell <kidusacc@gmail.com>
2024-07-19 18:29:47 +0300

    Add files via upload

---
+++ cakebet/main.js
@@ -0,0 +1,13 @@
+const toggleBtn = document.querySelector('.toggle_btn')
+const toggleBtnIcon = document.querySelector('.toggle_btn i')
+const dropDownMenu = document.querySelector('.dropdown_menu')
+
+toggleBtn.onclick = function (){
+    dropDownMenu.classList.toggle('open')
+    const isOpen = dropDownMenu.classList.contains('open')
+
+    toggleBtnIcon.classList = isOpen
+    ? ' bx bx-x'
+    : 'bx bx-menu ' 
+}
+
